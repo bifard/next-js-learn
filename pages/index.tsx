3bc4e7fc6f62b53c9af4bca3d01840c7a1c03f8d
Alexey Beloshapkin <Leks11172@mail.ru>
2023-04-03 00:25:00 +0700

    first component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { Htag } from "@/components";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
@@ -14,7 +15,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>sasad</div>
+      <div>
+        <Htag tag="h2">Text</Htag>
+      </div>
     </>
   );
 }
